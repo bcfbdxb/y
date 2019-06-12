03f29ae98512d6ceb5ff88f3d1ca85e18d6b400d
s20181106118 <s20181106118@mac28.local>
2019-06-12 20:33:11 +0800

    Initial Commit

---
+++ y/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  y
+//
+//  Created by s20181106118 on 2019/6/12.
+//  Copyright © 2019 s20181106118. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
